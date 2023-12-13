00f47a60ce5675ec47aa49dd5c2f48e97c37abbf
Bruno252 <143636176+Bruno252@users.noreply.github.com>
2023-12-13 17:51:57 -0300

    Atualizar o Biblioteca.c

--- Biblioteca.c
+++ Biblioteca.c
@@ -3,8 +3,26 @@
 #include <string.h>
 #include <locale.h>
 
-void a(){
+void pratileiraA() {
+    printf("Livro:a\n");
+    printf("Genero:a\n");
+    printf("Autor:a\n");
+}
+void pratileiraB() {
+    printf("Livro:b\n");
+    printf("Genero:b\n");
+    printf("Autor:b\n");
+
+}
+
+void a() {
     printf("---------------------------------\n");
+}
+
+void limpa()
+{
+    fflush(stdin);
+    system("cls || clear");
 }
 
 void menu()
@@ -14,22 +32,44 @@ void menu()
     printf("\n---------------------------------\n");
 }
 
-struct Gestaobiblioteca{
+void menu2()
+{
+    printf("\n---------------------------------------\n");
+    printf("\tEmprestimos De Livros");
+    printf("\n\nLivros Disponiveis: \n");
+    printf("\n1- Iracema");
+    printf("\n---------------------------------------\n");
+}
 
-    char nomelivro[200];
-    char generolivro[200];
-    char autorlivro[200];
+struct Gestaobiblioteca {
+
+    char nomelivroautor[250];
+    char nomelivro[50][200];
+    char generolivro[50][200];
+    char autorlivro[50][200];
     char editoralivro[200];
     char emailAutor[300];
+
+    float livroPreco[5];
+    float somaLivro;
+    float somaTotal;
+    int acumulador[5];
+    int livro1;
+    int livro2;
 
     char usuario[250];
     char senha[250];
     char usuarioSalvo[250];
     char senhaSalvo[250];
     int i;
+    int q[5];
+    int b;
+    int c;
 };
 
-void login(struct Gestaobiblioteca biblioteca){
+struct Gestaobiblioteca login(struct Gestaobiblioteca biblioteca) {
+    int i;
+    setbuf(stdin,NULL);
     a();
     printf("Usurname:\n");
     a();
@@ -39,77 +79,82 @@ void login(struct Gestaobiblioteca biblioteca){
     printf("Senha:\n");
     a();
     fgets(biblioteca.senha,sizeof(biblioteca.senha),stdin);
-    
-    if(strcmp(biblioteca.usuario,biblioteca.usuarioSalvo)==0 && strcmp(biblioteca.senha,biblioteca.senhaSalvo)==0){
-        a();
-        printf("Bem-Vindo\n");
-        a();
-        biblioteca.i++;
+
+
+    if(strcmp(biblioteca.usuario,biblioteca.usuarioSalvo)==0 && strcmp(biblioteca.senha,biblioteca.senhaSalvo)==0) {
+        biblioteca.i=1;
         //CadastroLivro(biblioteca);
-    }else{
-        printf("Error");
+    } else {
+        limpa();
+        printf("Username ou Senha errada\n");
     }
+    
+    return biblioteca;
 }
 
 
-void loginECadastro(struct Gestaobiblioteca biblioteca){
+void loginECadastro(struct Gestaobiblioteca biblioteca) {
 
     int escolha;
     //struct Gestaobiblioteca biblioteca;
-
-
-do{
     biblioteca.i=0;
-    a();
-    printf("1 | para cadastro\n");
-    printf("2 | para login\n");
-    a();
-    scanf("%d",&escolha);
+    do {
+        a();
+        printf("1 | para cadastro\n");
+        printf("2 | para login\n");
+        a();
+        scanf("%d",&escolha);
 
-    switch(escolha){
+        switch(escolha) {
 
         case 1:
-                limpa();
-                fflush(stdin);
-                a();
-                printf("Username:\n");
-                a();
-                fgets(biblioteca.usuarioSalvo,sizeof(biblioteca.usuarioSalvo),stdin);
-                a();
-                printf("Senha:\n");
-                a();
-                fgets(biblioteca.senhaSalvo,sizeof(biblioteca.senhaSalvo),stdin);
-                limpa();
-        break;
+            limpa();
+            setbuf(stdin,NULL);
+            fflush(stdin);
+            a();
+            printf("Username:\n");
+            a();
+            fgets(biblioteca.usuarioSalvo,sizeof(biblioteca.usuarioSalvo),stdin);
+            
+            a();
+            printf("Senha:\n");
+            a();
+            fgets(biblioteca.senhaSalvo,sizeof(biblioteca.senhaSalvo),stdin);
+            limpa();
+            biblioteca.b++;
+            break;
 
         case 2:
-                limpa();
-                fflush(stdin);
-                login(biblioteca);
-                limpa();
-        break; 
+            limpa();
+            fflush(stdin);
+            biblioteca=login(biblioteca);
+            break;
 
-    }
-    
-}while(escolha!=2 && biblioteca.i<1);
+        default:
+            limpa();
+            printf("Escolha Invalida\n");
+
+            break;
+
+        }
+
+    } while(biblioteca.i!=1);
+    limpa();
+    printf("Bem-Vindo\n");
 }
 
-void limpa()
-{
-    fflush(stdin);
-    system("cls || clear");
-}
 
-void CadastroLivro(struct Gestaobiblioteca biblioteca){
+void CadastroLivro(struct Gestaobiblioteca biblioteca) {
     //setlocale(LC_ALL, "portuguese");
-
+    fflush(stdin);
+    setbuf(stdin,NULL);
+    limpa();
     menu();
     printf("\nDigite o nome do livro que deseja cadastrar: ");
     fgets(biblioteca.nomelivro,sizeof(biblioteca.nomelivro),stdin);
 
     printf("Digite o genero do livro: ");
     gets(biblioteca.generolivro);
-
 
     printf("Digite o nome do autor do livro: ");
     gets(biblioteca.autorlivro);
@@ -134,43 +179,180 @@ void CadastroLivro(struct Gestaobiblioteca biblioteca){
 
 }
 
-void menu2()
-{
-    printf("\n---------------------------------------\n");
-    printf("\tEmprestimos De Livros");
-    printf("\n\nLivros Disponiveis: \n");
-    printf("\n1- Iracema");
-    printf("\n---------------------------------------\n");
-}
 
-void emprestimo()
+struct Gestaobiblioteca emprestimo(struct Gestaobiblioteca biblioteca)
 {
     //setlocale(LC_ALL, "portuguese");
 
     int escolha;
+    int escolha2;
+    int acumulador[6];
+    int i,q;
 
-    menu2();
-    printf("Digite o numero do livro que deseja: ");
-    scanf("%d", &escolha);
-    limpa();
-
-    switch (escolha)
-    {
-    case 1:
-        printf("Livro escolhido: Iracema\n");
-        printf("Prazo de devolção: 14 Dias");
-        break;
-    case 2:
-        printf("Livro escolhido: Dom Casmurro\n");
-        printf("Prazo de devolção: 14 Dias");
-        break;
-
-    default:
-        printf("Opção Invalida!");
-        break;
+    for(i=0; i<6; i++) {
+        acumulador[i]=0;
     }
 
-    
+    i=0;
+
+    do {
+        printf("-----------------------------------------------------------------\n");
+        printf("Digite 1 | para pedir um livro emprestado:\n");
+        printf("Digite 2 | para consultar livro emprestado e o prazo de entrega\n");
+        printf("Digite 3 | para sair\n");
+        printf("------------------------------------------------------------------\n");
+        scanf("%d", &escolha);
+        limpa();
+
+        switch (escolha)
+        {
+        case 1:
+            setbuf(stdin,NULL);
+            printf("Opcoes Disponiveis:\n");
+            a();
+            printf("Numero do Livro:1\n");
+            pratileiraA();
+            a();
+            printf("Numero do Livro:2\n");
+            pratileiraB();
+            a();
+            printf("Numero do Livro:");
+            scanf("%d",&escolha2);
+            limpa();
+
+            if(biblioteca.q[escolha2]<1) {
+                if(escolha2>=1 && escolha2<3) {
+                    biblioteca.q[escolha2]++;
+                    biblioteca.acumulador[biblioteca.i]=escolha2;
+                }
+            } else {
+                limpa();
+                a();
+                printf("Este Livro ja foi emprestado\n");
+                a();
+            }
+            //printf("Nome do livro:");
+            //fgets(biblioteca.nomelivro[biblioteca.i],sizeof(biblioteca.nomelivro[biblioteca.i]),stdin);
+
+            biblioteca.i++;
+            break;
+
+        case 2:
+            for(q=0; q<5; q++) {
+
+                if(biblioteca.acumulador[q]==1) {
+                    a();
+                    pratileiraA();
+                    a();
+                }
+
+                if(biblioteca.acumulador[q]==2) {
+                    a();
+                    pratileiraB();
+                    a();
+                }
+
+                // printf("Nome do Livro:%s",biblioteca.nomelivro[q]);
+                //printf("Prazo ate 7 dias para devolucao\n");
+                //printf("\n");
+            }
+            printf("Prazo ate 7 dias para devolucao\n");
+            break;
+
+        case 3:
+
+            break;
+
+        default:
+            printf("Opção Invalida!");
+            break;
+        }
+
+    } while(escolha!=3);
+    return biblioteca;
+}
+
+struct Gestaobiblioteca comprar(struct Gestaobiblioteca biblioteca) {
+
+    int escolha;
+    int escolha2;
+    int acumulador[2];
+    int livro1=0;
+    int livro2=0;
+    int i=0;
+    float soma;
+
+    do {
+        a();
+        printf("1 | para por uma livro no carrinho\n");
+        printf("2 | para ver o carrinho\n");
+        printf("3 | para voltar ao menu\n");
+        a();
+        scanf("%d",&escolha);
+        switch(escolha) {
+
+        case 1:
+            limpa();
+            a();
+            printf("Numero do Livro:1\n");
+            pratileiraA();
+            printf("Preco:%2.f\n",biblioteca.livroPreco[1]);
+            
+            a();
+            printf("Numero do Livro:2\n");
+            pratileiraB();
+            printf("Preco:%2.f\n",biblioteca.livroPreco[2]);
+            a();
+            scanf("%d",&escolha2);
+
+            acumulador[i]=escolha2;
+
+            if(acumulador[i]==1) {
+                biblioteca.livro1++;
+            } else if(acumulador[i]==2) {
+                biblioteca.livro2++;
+            }
+
+            biblioteca.somaLivro+=biblioteca.livroPreco[escolha2];
+            limpa();
+            printf("Valor Total antes da compra:%.2f\n",biblioteca.somaLivro);
+            break;
+
+        case 2:
+        if(biblioteca.livro1>0 || biblioteca.livro2>0){
+            if(biblioteca.livro1>0) {
+                limpa();
+                a();
+                pratileiraA();
+                printf("Preco:%2.f\n",biblioteca.livroPreco[1]);
+                printf("Quantidade:%d\n",biblioteca.livro1);
+                a();
+
+            } 
+                if(biblioteca.livro2>0){
+                a();
+                pratileiraB();
+                printf("Preco:%2.f\n",biblioteca.livroPreco[2]);
+                printf("Quantidade:%d\n",biblioteca.livro2);
+                a();
+
+            }
+            printf("Valor Total da Compra:%.2f\n",biblioteca.somaLivro);
+            }else{
+            limpa();
+            printf("Nenhum livro foi comprado\n");
+            
+            }
+            
+            break;
+
+        case 3:
+            limpa();
+            break;
+
+        }
+    } while(escolha!=3);
+    return biblioteca;
 }
 
 void opcoes()
@@ -179,56 +361,64 @@ void opcoes()
     printf("\tEscolha Opçao desejada");
     printf("\n1- Cadastro de Livros\n");
     printf("2- Emprestimo de livro\n");
-    printf("3- para ver prazo de entrega do livro\n");
+    printf("3- Comprar Livro\n");
     printf("4- deslogar e sair");
     printf("\n------------------------------------\n");
 }
 
-void prazo(struct Gestaobiblioteca biblioteca){
-    printf("Nome do Livro:Iracema");
-    printf("Prazo de entrega:%d",7);
-}
 
-int main(){
+int main() {
     setlocale(LC_ALL, "portuguese");
     int escolha;
+    int i;
     struct Gestaobiblioteca biblioteca;
     loginECadastro(biblioteca);
-    
-    do{
 
-    opcoes();
-    printf("Escolha:");
-    scanf("%d",&escolha);
 
-    switch(escolha){
+    biblioteca.livroPreco[1]=50;
+    biblioteca.livroPreco[2]=100;
+    biblioteca.somaLivro=0;
+    biblioteca.i=0;
+    biblioteca.livro1=0;
+    biblioteca.livro2=0;
+    biblioteca.b=0;
 
-        case 1:
-        fflush(stdin);
-        CadastroLivro(biblioteca);
-        break;
-
-        case 2:
-        fflush(stdin);
-        emprestimo();
-        break;
-
-        case 3:
-        prazo(biblioteca);
-
-        break;
-
-        case 4:
-        
-        break;
-
-        default:
-        printf("Escolha Invalida");
-        break;
+    for(i=0; i<4; i++) {
+        biblioteca.q[i]=0;
     }
 
-    }while(escolha!=4);
-    printf("Obrigado e Volte Sempre");
+    do {
+
+        opcoes();
+        printf("Escolha:");
+        scanf("%d",&escolha);
+
+        switch(escolha) {
+
+        case 1:
+            fflush(stdin);
+            CadastroLivro(biblioteca);
+            break;
+
+        case 2:
+            fflush(stdin);
+            limpa();
+            biblioteca=emprestimo(biblioteca);
+            break;
+
+        case 3:
+            fflush(stdin);
+            limpa();
+            biblioteca=comprar(biblioteca);
+
+            break;
+
+        default:
+            printf("Escolha Invalida");
+            break;
+        }
+
+    } while(escolha!=4);
+    printf("Obrigado e Volte Sempre\n");
     return 0;
 }
-
